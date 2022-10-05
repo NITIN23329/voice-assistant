b78f02ada1d0aca38f2c3a1cbc9a8ac98fc13686
nitin23329 <NITIN23329@GMAIL.COM>
2022-10-05 13:10:26 +0530

    added css for buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
-import React, { Component }  from 'react';
+import React, { Component, useEffect, useState }  from 'react';
 import './App.css';
+import { Container  } from 'react-dom';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const App = () => {
   const {
     transcript,
     listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable
   } = useSpeechRecognition();
 
-  if (!browserSupportsSpeechRecognition) {
+  const [userTranscript, setUserTranscipt]  = useState("")
+  const recipeDBLogo = 'https://cosylab.iiitd.edu.in/recipedb/static/media/recipedb-logo.png'
+
+  useEffect(()=>{
+    setUserTranscipt(transcript)
+  },[transcript])
+
+  if (!browserSupportsSpeechRecognition || !isMicrophoneAvailable) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
+  
+  function handleChange(event){
+      setUserTranscipt(event.target.value)
+  }
+
+  function handleSubmit(event){
+    console.log("user spoked: "+event.target.value);
+  }
+
+  function resetTranscript(){
+    SpeechRecognition.stopListening()
+    setUserTranscipt("");
+  }
+
+
 
   return (
     <div>
-      <h1> Testing</h1>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
-      <button onClick={SpeechRecognition.startListening}>Start</button>
-      <button onClick={SpeechRecognition.stopListening}>Stop</button>
-      <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
+        <img src = {recipeDBLogo} className = "recipeDBLogo"></img>
+        <h1 className='header'> Voice Assistant for RecipeDB</h1>
+        <br/>
+        <br/>
+
+      <div className='left-margin'>
+        <h4 >A resource for exploring recipes</h4>
+      </div>
+
+      <div >
+      <button className='buttonstyle buttonmargin1' onClick={SpeechRecognition.startListening}>SPEAK</button>
+        <button className='buttonstyle buttonmargin1' onClick={resetTranscript}>CLEAR</button>
+      </div>
+
+      <h2 className='left-margin'>{listening ? 'Listening...' : ''}</h2>
+       
+    
+      {/* <button onClick={SpeechRecognition.stopListening}>Stop</button> */}
+      <form onSubmit={handleSubmit} className='centerdiv'>
+        <label >
+          <textarea type="text" value={userTranscript} onChange={handleChange} placeholder="Click SPEAK Button to speak something..." className='textareastyle'/>
+        </label>
+        <button className='buttonstyle buttonmargin2' onClick={handleSubmit}>SUBMIT</button> 
+      </form>
     </div>
   );
 };
